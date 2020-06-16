b9a8f47d5e71d6b463cb18ba236f05a55c951cc8
Nikos Kougianos <kougi_jr@hotmail.com>
2020-06-16 12:12:42 +0300

    Arrays: Left rotation

---
+++ rotateLeft.py
@@ -0,0 +1,18 @@
+# Arrays: Left rotation
+# Quiz URL:
+# 	https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+# Quiz description:
+# 	A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+
+# 	Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+d = 4
+a = [3,7,1,8,0,5]
+print(a)
+
+def rotLeft(a, d):
+	return a[d:len(a)] + a[0:d]
+
+result = rotLeft(a,d)
+
+print(result)
